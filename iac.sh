b34f40c71ca68e8974ebda932519a22607633497
FllavioAndrade <flavio.a.s@live.com>
2022-08-18 16:04:30 +0000

    Adicionado o comando para criar o diretório home de todos os usuario

--- iac.sh
+++ iac.sh
@@ -8,14 +8,14 @@ groupadd GRP_SEC
 
 echo "Criando Usuários"
 useradd carlos -c "Carlos" -m -s /bin/bash -g GRP_ADM -p $(openssl passwd -crypt mudar123)       
-useradd maria -c "Maria" -s /bin/bash -g GRP_ADM -p $(openssl passwd -crypt mudar123)
-useradd joao -c "Joao" -s /bin/bash -g GRP_ADM -p $(openssl passwd -crypt mudar123)
-useradd debora -c "Debora" -s /bin/bash -g GRP_VEN -p $(openssl passwd -crypt mudar123)
-useradd sebastiana -c "sebastiana" -s /bin/bash -g GRP_VEN -p $(openssl passwd -crypt mudar123)  
-useradd roberto -c "roberto" -s /bin/bash -g GRP_VEN -p $(openssl passwd -crypt mudar123)        
-useradd josefina -c "josefina" -s /bin/bash -g GRP_SEC -p $(openssl passwd -crypt mudar123)      
-useradd amanda -c "amanda" -s /bin/bash -g GRP_SEC -p $(openssl passwd -crypt mudar123)
-useradd rogerio -c "rogerio" -s /bin/bash -g GRP_SEC -p $(openssl passwd -crypt mudar123)        
+useradd maria -c "Maria" -m -s /bin/bash -g GRP_ADM -p $(openssl passwd -crypt mudar123)
+useradd joao -c "Joao" -m -s /bin/bash -g GRP_ADM -p $(openssl passwd -crypt mudar123)
+useradd debora -c "Debora" -m -s /bin/bash -g GRP_VEN -p $(openssl passwd -crypt mudar123)
+useradd sebastiana -c "sebastiana" -m -s /bin/bash -g GRP_VEN -p $(openssl passwd -crypt mudar123)  
+useradd roberto -c "roberto" -m -s /bin/bash -g GRP_VEN -p $(openssl passwd -crypt mudar123)        
+useradd josefina -c "josefina" -m -s /bin/bash -g GRP_SEC -p $(openssl passwd -crypt mudar123)      
+useradd amanda -c "amanda" -m -s /bin/bash -g GRP_SEC -p $(openssl passwd -crypt mudar123)
+useradd rogerio -c "rogerio" -m -s /bin/bash -g GRP_SEC -p $(openssl passwd -crypt mudar123)        
 
 echo "Atribuindo permissões aos usuários"
 chown root:GRP_ADM /adm
